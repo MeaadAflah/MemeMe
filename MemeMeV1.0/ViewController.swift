9d992fd0bfd15fc94386a6cfc82fd95fc8f46b3d
SuperMadio <30291256+SuperMadio@users.noreply.github.com>
2018-11-17 13:56:06 +0300

    Initial Commit

---
+++ MemeMeV1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMeV1.0
+//
+//  Created by Super Madio on 09/03/1440 AH.
+//  Copyright © 1440 Super Madio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
